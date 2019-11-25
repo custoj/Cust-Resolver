a1675787cc57ec7890a3179a6f89ea25e4d4d2e8
ccw630 <ccw630@gmail.com>
2019-11-25 23:16:00 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# 某个能用的*cpc滚榜
+
+## Features
+
+* 基于的版本
+  * 1.1.0dev.1057
+
+* 用到的数据格式
+  * 具体见`sample.xml`
+
+* 中文字体
+  * 使用了自己的mac里不知道哪里掏出来的`STHEITI.ttf`，其他的显示都没这个正常
+  * 要转成`pfb`格式，替换`lib/resolver.jar`里的`font/HELV.PFB`
+  * 但是还有某些字符不能正常显示，所以先把自带的`HELV.PFB`捞出来转成`Helvetica.ttf`，和`STHEITI.ttf`合并一下，再转回`pfb`格式。个人感觉`Helvetica`有点古老，所以是`right join`~
+
+## Usage
+
+直接上服务器操库，分别跑`query.sql`里的两句话。执行方法如下
+```
+\o PATH_TO_FILE
+...
+\o
+```
+两句话分别对应的是所有的`team`和所有的`run`，格式见`sample.xml`。注意`award`需要自己手动随便处理一下。
+
+## Bugs
+
+* 貌似排名只支持分钟级
---
+++ query.sql
@@ -0,0 +1,18 @@
+select xmlforest(xmlforest(u.id as id,concat(p.real_name,' - ',p.school)as university)as team) from "user" u
+join user_profile p on p.user_id=u.id
+where u.username~'team71\d{3}'
+
+select xmlforest(xmlforest(row_number() over (order by s.create_time) as id,ascii(p._id)-64 as problem,extract(epoch from s.create_time-c.start_time) as time,s.user_id as team,true as judged,case when s.result=0 then true else false end as solved,case when s.result=0 or s.result=-2 then false else true end as penalty,case when f.is_first_solve=true then true else false end as "first-to-solve")as run) from submission s
+join "user" u on u.id=user_id
+join problem p on p.id=s.problem_id
+join contest c on c.id=s.contest_id
+left join (
+    select problem_id,min(s.create_time)as first_solve_time,true as is_first_solve from submission s
+    join "user" u on u.id=user_id
+    where contest_id=6
+    and result=0
+    and admin_type='Regular User'
+    group by problem_id
+)f on s.create_time=f.first_solve_time
+where s.contest_id=6
+and u.admin_type='Regular User'

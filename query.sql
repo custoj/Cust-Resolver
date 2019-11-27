936f527e3ca76e4ea0afcf0718194df7c1db4fc7
ccw630 <ccw630@gmail.com>
2019-11-27 21:58:10 +0800

    remove teams without submission

--- query.sql
+++ query.sql
@@ -1,6 +1,8 @@
 select xmlforest(xmlforest(u.id as id,concat(p.real_name,' - ',p.school)as university)as team) from "user" u
 join user_profile p on p.user_id=u.id
+join submission s on s.user_id=u.id
 where u.username~'team71\d{3}'
+and contest_id=6
 union all
 select xmlforest(xmlforest(row_number() over (order by s.create_time) as id,ascii(p._id)-64 as problem,extract(epoch from s.create_time-c.start_time) as time,s.user_id as team,true as judged,case when s.result=0 then true else false end as solved,case when s.result=0 or s.result=-2 then false else true end as penalty,case when f.is_first_solve=true then true else false end as "first-to-solve")as run) from submission s
 join "user" u on u.id=user_id
